e436caec48571ea9b701eae73c034cc9be3d626a
jagtapK <jagtapkajal54@gmail.com>
2025-08-15 22:32:53 +0530

    fix method

--- src/main/java/com/company/service/ExcelService.java
+++ src/main/java/com/company/service/ExcelService.java
@@ -2,6 +2,10 @@ package com.company.service;
 
 import com.company.entity.Developer;
 import com.company.repository.DeveloperRepository;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.poifs.crypt.EncryptionInfo;
+import org.apache.poi.poifs.crypt.Encryptor;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -10,15 +14,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 @Service
-public class ExcelService {
+public abstract class ExcelService {
 
     @Autowired
     private DeveloperRepository developerRepository;
 
-    public void saveExcelData(MultipartFile file) throws IOException {
+    public String saveExcelData(MultipartFile file) throws IOException {
         try (Workbook workbook = new XSSFWorkbook(file.getInputStream())) {
             Sheet sheet = workbook.getSheetAt(0);
 
@@ -42,5 +49,31 @@ public class ExcelService {
         } catch (IOException e) {
             throw new RuntimeException("Failed to Add excel file ");
         }
+        // After saving to DB, create password-protected copy
+        try {
+            POIFSFileSystem fs = new POIFSFileSystem();
+            EncryptionInfo info = new EncryptionInfo(fs);
+            Encryptor enc = info.getEncryptor(); // ✅ correct class name
+            String password = "Excel@123";
+            enc.confirmPassword(password);
+
+            try (OPCPackage opc = OPCPackage.open(file.getInputStream());
+                 OutputStream os = enc.getDataStream(fs)) {
+                opc.save(os);
+            }
+
+            File protectedFile = new File("protected_" + file.getOriginalFilename());
+            try (FileOutputStream fos = new FileOutputStream(protectedFile)) {
+                fs.writeFilesystem(fos);
+            }
+
+            return "Excel data saved to DB and protected file created at: " + protectedFile.getAbsolutePath();
+
+        } catch (Exception e) {
+            throw new RuntimeException("Data saved to DB, but failed to create password-protected file: " + e.getMessage());
+        }
+
     }
+
+    public abstract String uploadAndProtectExcel(MultipartFile file, String password) throws Exception;
 }
