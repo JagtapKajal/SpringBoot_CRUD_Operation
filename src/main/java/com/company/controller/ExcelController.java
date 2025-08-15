084a4441b60d497cb9308ed04f54d596c3f847b0
jagtapK <jagtapkajal54@gmail.com>
2025-08-15 21:50:26 +0530

    create new class to protect Excel file

--- src/main/java/com/company/controller/ExcelController.java
+++ src/main/java/com/company/controller/ExcelController.java
@@ -1,22 +1,28 @@
 package com.company.controller;
 
+import com.company.entity.Developer;
+import com.company.repository.DeveloperRepository;
 import com.company.service.ExcelService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.antlr.v4.runtime.tree.pattern.ParseTreePattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 public class ExcelController {
 
     @Autowired
     private ExcelService excelService;
+
+    @Autowired
+    private DeveloperRepository developerRepository;
 
     @Operation(summary = "Upload Excel File")
     @ApiResponses(value = {
@@ -34,4 +40,10 @@ public class ExcelController {
             return "Error : Can not upload data : " + e.getMessage();
         }
     }
+    @GetMapping("/all")
+    public ResponseEntity<List<Developer>> getAllFromExcel() {
+
+        return ResponseEntity.ok(developerRepository.findAll());
+    }
+
 }
--- src/main/java/com/company/serviceImpl/ExcelServiceImpl.java
+++ src/main/java/com/company/serviceImpl/ExcelServiceImpl.java
@@ -1,32 +1,4 @@
 package com.company.serviceImpl;
 
-import com.company.entity.Developer;
-import com.company.helper.ExcelHelper;
-import com.company.repository.DeveloperRepository;
-import com.company.service.ExcelService;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.List;
-
-@Service
-public class ExcelServiceImpl implements ExcelService {
-
-    private final DeveloperRepository developerRepository;
-
-    public ExcelServiceImpl(DeveloperRepository developerRepository) {
-        this.developerRepository = developerRepository;
-    }
-
-    @Override
-    public void save(MultipartFile file) {
-        try {
-            List<Developer> developers = ExcelHelper.convertExcelTOListDeveloper(file.getInputStream());
-            developerRepository.saveAll(developers);
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to store Excel data: " + e.getMessage());
-        }
-    }
+public class ExcelServiceImpl {
 }
-
