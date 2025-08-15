c659024fcc8ff4f47801c3fecb32bed6090d49bd
jagtapK <jagtapkajal54@gmail.com>
2025-08-15 22:33:21 +0530

    implement logic for excel protection

--- src/main/java/com/company/serviceImpl/ExcelServiceImpl.java
+++ src/main/java/com/company/serviceImpl/ExcelServiceImpl.java
@@ -1,4 +1,40 @@
 package com.company.serviceImpl;
 
-public class ExcelServiceImpl {
+import com.company.service.ExcelService;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.poifs.crypt.EncryptionInfo;
+import org.apache.poi.poifs.crypt.Encryptor;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+@Service
+public class ExcelServiceImpl extends ExcelService {
+
+    @Override
+    public String uploadAndProtectExcel(MultipartFile file, String password) throws Exception {
+        // Create POI filesystem for encryption
+        POIFSFileSystem fs = new POIFSFileSystem();
+        EncryptionInfo info = new EncryptionInfo(fs);
+        Encryptor enc = info.getEncryptor();
+        enc.confirmPassword(password);
+
+        // Write the uploaded file content into encrypted stream
+        try (OPCPackage opc = OPCPackage.open(file.getInputStream());
+             OutputStream os = enc.getDataStream(fs)) {
+            opc.save(os);
+        }
+
+        // Save to disk (you can save to DB, S3, etc.)
+        File protectedFile = new File("protected_" + file.getOriginalFilename());
+        try (FileOutputStream fos = new FileOutputStream(protectedFile)) {
+            fs.writeFilesystem(fos);
+        }
+
+        return "File saved at: " + protectedFile.getAbsolutePath();
+    }
 }
